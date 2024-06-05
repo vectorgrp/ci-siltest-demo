5d1cc5cafd23c6fbd46f638493f2d6b448ca6a38
SouhailAssal2 <156662195+SouhailAssal2@users.noreply.github.com>
2024-06-05 16:40:55 +0200

    remove unnecessary files (#44)
    * remove unnecessary files
    
    * clean up Simulink folder
    
    * remove unneeded files
    
    * Move Simulink folder

--- ECU/Appl/Source/Main.c
+++ ECU/Appl/Source/Main.c
@@ -1,20 +1,10 @@
 #include "EcuM.h"
 #include "Os.h"
 
-int main(void)
-{
-  EcuM_Init();    
-  return 0;
-}
 
 TASK(InitTask)
 {
   EcuM_StartupTwo();    
 
   TerminateTask();
-}
-
-boolean Appl_DetEntryCallout(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId)
-{
-  return FALSE;
 }
