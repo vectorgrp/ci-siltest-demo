6793f5de4ae0183b04272d06aed96cc0aefd26e9
Peter <133140762+PeterKoegel@users.noreply.github.com>
2024-09-16 15:22:12 +0200

    Update LightCtrl_SWC.c

--- ECU/Appl/Source/LightCtrl_SWC.c
+++ ECU/Appl/Source/LightCtrl_SWC.c
@@ -57,6 +57,7 @@ void LightCtrl(void)
   /* During: BC/Chart */
 
   // rtb_LightIntensity_LightIntensi = 0;
+  Input_LightSwitch_local_storage = 0;
 
   if (LightCtrl_SWC_DW.is_active_c1_LightCtrl_SWC == 0U) {
     /* Entry: BC/Chart */
