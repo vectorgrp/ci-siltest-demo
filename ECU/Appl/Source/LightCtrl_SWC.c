0a0cac9afea1b5726949be05730d09a79c7a0342
David Koch <david.koch@vector.com>
2025-02-25 16:21:50 +0100

    Update LightCtrl_SWC.c

--- ECU/Appl/Source/LightCtrl_SWC.c
+++ ECU/Appl/Source/LightCtrl_SWC.c
@@ -56,8 +56,9 @@ void LightCtrl(void)
   /* Gateway: BC/Chart */
   /* During: BC/Chart */
 
-  rtb_LightIntensity_LightIntensi = 0;
+  // rtb_LightIntensity_LightIntensi = 0;
 
+  
   if (LightCtrl_SWC_DW.is_active_c1_LightCtrl_SWC == 0U) {
     /* Entry: BC/Chart */
     LightCtrl_SWC_DW.is_active_c1_LightCtrl_SWC = 1U;
